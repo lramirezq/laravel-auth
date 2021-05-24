cef44b754f3c2bb2d6125b4309fa0967029289c6
Luis Ramirez Queupul <lramirez@1mz3lt2.proteccion.local>
2021-05-24 18:50:11 -0400

    abriendo el index

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth/login');
+ //   return view('auth/login');
+ return view('/');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
